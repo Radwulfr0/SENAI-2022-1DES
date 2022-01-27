615deaf0e44c7e6330d40b7f86d3a55f3e470741
ReenyeLima <52282158+ReenyeLima@users.noreply.github.com>
2022-01-27 10:20:29 -0200

    realitas

---
+++ FPOO/Work2/Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis{
+	public static void main(String[] args){
+	    byte byte1 = 65;
+		byte byte2 = 66;
+		int resultado = byte1 + byte2;
+		System.out.println(resultado);//soma1
+		char char1 = 65;
+		char char2 = 66;
+		String result = char1 + " " + char2;
+		System.out.println(result);//soma 2
+		System.out.println("A soma de dois bytes = " + resultado);
+		System.out.println("A soma de dois caracteres = " + result);
+    }
+}
